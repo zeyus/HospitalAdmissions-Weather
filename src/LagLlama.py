b3f903afd686b314a26398b395a6c1bd285bebce
zeyus <zeyus@zeyus.com>
2024-05-26 21:34:09 +0200

    Lagllama wip

--- src/LagLlama.py
+++ src/LagLlama.py
@@ -1,35 +1,37 @@
+# Code adapted from: https://colab.research.google.com/drive/1DRAzLUPxsd-0r8b-o4nlyFXrjw_ZajJJ?usp=sharing#scrollTo=vPBAO18DWT8A
+# https://github.com/time-series-foundation-models/lag-llama
+
+
 from itertools import islice
+import logging
 
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 
 import torch
-from gluonts.evaluation import make_evaluation_predictions, Evaluator
-from gluonts.dataset.repository.datasets import get_dataset
+from gluonts.evaluation import make_evaluation_predictions, Evaluator  # type: ignore
+from gluonts.dataset.repository.datasets import get_dataset  # type: ignore
 
-from gluonts.dataset.pandas import PandasDataset
+from gluonts.dataset.pandas import PandasDataset  # type: ignore
 import pandas as pd
+from tqdm import tqdm
+
+from common.config import DATA_DIR, FIGURE_DIR, read_data, prepare_data, MODEL_DIR
 
 # add contrib/lag-llama to the path
 import sys
-sys.path.append("contrib/lag_llama")
+sys.path.append("contrib/lag-llama")
 from lag_llama.gluon.estimator import LagLlamaEstimator
 
 
-
-def get_lag_llama_predictions(dataset, prediction_length, device, context_length=32, use_rope_scaling=False, num_samples=100):
-    ckpt = torch.load("lag-llama.ckpt", map_location=device) # Uses GPU since in this Colab we use a GPU.
+def get_lag_llama_predictions(dataset, prediction_length, context_length=32, num_samples=20, device="cuda", batch_size=64, nonnegative_pred_samples=True):
+    ckpt = torch.load("./contrib/hf_model/lag-llama.ckpt", map_location=device)
     estimator_args = ckpt["hyper_parameters"]["model_kwargs"]
 
-    rope_scaling_arguments = {
-        "type": "linear",
-        "factor": max(1.0, (context_length + prediction_length) / estimator_args["context_length"]),
-    }
-
     estimator = LagLlamaEstimator(
-        ckpt_path="lag-llama.ckpt",
+        ckpt_path="./contrib/hf_model/lag-llama.ckpt",
         prediction_length=prediction_length,
-        context_length=context_length, # Lag-Llama was trained with a context length of 32, but can work with any context length
+        context_length=context_length,
 
         # estimator args
         input_size=estimator_args["input_size"],
@@ -38,11 +40,17 @@ def get_lag_llama_predictions(dataset, prediction_length, device, context_length
         n_head=estimator_args["n_head"],
         scaling=estimator_args["scaling"],
         time_feat=estimator_args["time_feat"],
-        rope_scaling=rope_scaling_arguments if use_rope_scaling else None,
 
-        batch_size=1,
-        num_parallel_samples=100,
-        device=device,
+        nonnegative_pred_samples=nonnegative_pred_samples,
+
+        # linear positional encoding scaling
+        rope_scaling={
+            "type": "linear",
+            "factor": max(1.0, (context_length + prediction_length) / estimator_args["context_length"]),
+        },
+
+        batch_size=batch_size,
+        num_parallel_samples=num_samples,
     )
 
     lightning_module = estimator.create_lightning_module()
@@ -54,7 +62,69 @@ def get_lag_llama_predictions(dataset, prediction_length, device, context_length
         predictor=predictor,
         num_samples=num_samples
     )
-    forecasts = list(forecast_it)
-    tss = list(ts_it)
+    forecasts = list(tqdm(forecast_it, total=len(dataset), desc="Forecasting batches"))
+    tss = list(tqdm(ts_it, total=len(dataset), desc="Ground truth"))
 
     return forecasts, tss
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f'Using device: {device}')
+    # Load the data
+    logging.info('Loading data')
+    data = read_data()
+    data, _ = prepare_data(data)
+    
+
+    target = 'COVID-19 admissions (suspected and confirmed)'
+    features = [
+        target,
+        # 'day_of_week',
+        # 'humidity',
+        # 'temp',
+        # 'windspeed',
+        # 'winddir'
+    ]
+
+    data = data[features]
+    data = data.rename(columns={target: 'target'})
+    
+    # Add a pseudo item_id column
+    data['item_id'] = "Covid Admissions"
+
+    print(data.head())
+    data['target'] = data['target'].astype('float32')
+
+    # Create a PandasDataset from the data
+    logging.info('Creating dataset')
+    dataset = PandasDataset.from_long_dataframe(data, target="target", item_id="item_id")
+
+    prediction_length = 7
+    context_length = 8
+    num_samples = 100
+
+    # Get the forecasts
+    logging.info('Getting forecasts')
+    forecasts, tss = get_lag_llama_predictions(dataset, prediction_length, context_length=context_length, num_samples=num_samples, device=device)
+
+    # Plot the forecasts
+    logging.info('Plotting forecasts')
+    plt.figure(figsize=(20, 15))
+    date_formater = mdates.DateFormatter('%b, %d')
+    plt.rcParams.update({'font.size': 15})
+    for idx, (forecast, ts) in islice(enumerate(zip(forecasts, tss)), 9):
+        ax = plt.subplot(3, 3, idx + 1)
+        
+        plt.plot(ts[-4 * prediction_length:].to_timestamp(), label="target", )
+        forecast.plot( color='g')
+        plt.xticks(rotation=60)
+        ax.xaxis.set_major_formatter(date_formater)
+        ax.set_title(forecast.item_id)
+
+    plt.gcf().tight_layout()
+    plt.legend()
+
+    logging.info('Saving figure')
+    plt.savefig(FIGURE_DIR / "lag_llama_predictions.png")
